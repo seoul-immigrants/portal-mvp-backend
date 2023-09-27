e97825e74f0b38875887cd16555817fd2599a71f
Woojoo JUNG <jwj5045@gmail.com>
2023-09-27 15:07:22 +0900

    feat: 첫 커밋

---
+++ src/main/java/com/portal/mvp/PortalMvpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portal.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalMvpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalMvpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portal/mvp/PortalMvpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalMvpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
